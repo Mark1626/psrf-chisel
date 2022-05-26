e567480e6f9fe8f30bc0ad87b5962c8ec81d5148
Arun <arunkumarmv1997@gmail.com>
2022-05-26 12:23:50 +0530

    Add decision tree fsm

---
+++ src/main/scala/psrf/DecisionTree.scala
@@ -0,0 +1,40 @@
+package psrf
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.FixedPoint
+
+class DecisionTree(numFeatures: Int, numNodes: Int, FPWidth: Int, FPBinaryPoint: Int) extends Module {
+  val nodeAddrWidth = log2Ceil(numNodes)
+
+  val io = IO(new Bundle {
+    val in  = Flipped(Decoupled(Vec(numFeatures, FixedPoint(FPWidth.W, FPBinaryPoint.BP))))
+    val out = Irrevocable(Bool())
+  })
+
+  def getDecisionTreeRom(): Vec[Node] = ???
+  val decisionTreeRom = getDecisionTreeRom()
+
+  val idle :: busy :: Nil = Enum(2)
+
+  val state = RegInit(idle)
+  val start = io.in.valid & io.in.ready
+  val done  = WireInit(false.B)
+  val rest  = io.out.valid & io.out.ready
+
+  val out      = Reg(Bool())
+  val outValid = Reg(Bool())
+
+  // FSM
+  when(state === idle) {
+    io.in.ready := true.B
+    outValid    := false.B
+    when(start) {
+      state := busy
+    }
+  }.elsewhen(state === busy) {
+    when(rest) {
+      state := idle
+    }
+  }
+}
