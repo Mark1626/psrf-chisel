e2da6934c13a29030fe9b4b1ad36509da16f8640
Arunkumar M V <arunkmv@Arukumars-MBP.bbrouter>
2022-09-16 16:16:38 +0530

    Add presentation slides

--- docs/schematics/DecisionTree/gen-arch.sh
+++ docs/schematics/DecisionTree/gen-arch.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-if ! command -v ditaa &> /dev/null
-then
-    echo "Ditaa could not be found."
-    exit
-fi
-
-echo "Generating architecture schematic."
-ditaa arch.txt arch.png -E
-
 if ! command -v plantuml &> /dev/null
 then
     echo "Plantuml could not be found."
